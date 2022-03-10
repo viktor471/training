ea0221c5986b69ea3f5a490d013eb49955b7da6a
Viktor Borodin <vikbrdin@gmail.com>
2022-03-10 19:25:06 +0300

    Modify scopes

--- python/testing/pytests/scopes/scopes.py
+++ python/testing/pytests/scopes/scopes.py
@@ -1,1 +1,36 @@
 import pytest
+
+import json
+import logging
+from datetime import datetime
+
+
+@pytest.fixture()
+def dif_time():
+    print()
+    print("fixture dif_time")
+    return datetime.now()
+
+
+def test_1(dif_time):
+    print(dif_time)
+
+def test_2(dif_time):
+    print(dif_time)
+
+@pytest.fixture(scope="class")
+def scope_class_time():
+    print()
+    print("fixture scope_class_time")
+    return datetime.now()
+
+@pytest.mark.usefixtures("scope_class_time")
+class TestCalculatorClass:
+    def test_a(self):
+        print(scope_class_time)
+        print("test_a")
+
+    def test_b(self):
+        print(scope_class_time)
+        print("test_b")
+
