cf8cbef6c928c93898c88ad36d28831bce89145a
viktor borodin <vikbrdin@gmail.com>
2022-03-12 16:38:05 +0300

    delete docstring from pytests/with_conftest/test_external_dsp_direct.py

--- python/testing/pytests/with_conftest/test_external_dsp_direct.py
+++ python/testing/pytests/with_conftest/test_external_dsp_direct.py
@@ -1,10 +1,6 @@
 import pytest
 
 class TestPrepare:
-    """
-    Этот тест -- и не тест вовсе: его нужно выполнить, чтоб заполнить БД всеми необходимыми сущностями для работы биддера
-    и для его тестирования.
-    """
     @pytest.mark.manual
     def test_fill_db(self, db, prepare_db, httpserver_listen_address):
         print("manual test fill bd")
